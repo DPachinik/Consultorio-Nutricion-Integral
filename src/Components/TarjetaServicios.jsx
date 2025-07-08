56476461cc2b3a8f4cd7e8e9041285c243d8c8ce
David Pachinik <ing.pachinik96@gmail.com>
2025-07-08 14:59:16 -0400

    feat(ui):restyle card service

--- src/Components/TarjetaServicios.jsx
+++ src/Components/TarjetaServicios.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FiX } from 'react-icons/fi';
 
 const TarjetaServicios = ({ tratamientos }) => {
   const [description, setDescription] = useState(
     new Array(tratamientos.length).fill(false)
   );
 
-  const handleMouseEnter = (index) => {
+  const handleClickEnter = (index) => {
     const newDescription = [...description];
     newDescription[index] = true;
     setDescription(newDescription);
   };
-  const handleMouseLeave = (index) => {
+
+  const handleCloseClick = (index) => {
     const newDescription = [...description];
     newDescription[index] = false;
     setDescription(newDescription);
@@ -22,8 +24,6 @@ const TarjetaServicios = ({ tratamientos }) => {
       {tratamientos.map((servicio, index) => (
         <div
           key={index}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseLeave={() => handleMouseLeave(index)}
           className={`relative flex h-[150px] w-[350px] items-center justify-center rounded-lg bg-cover bg-center shadow-md shadow-primario transition-all duration-1000 hover:cursor-pointer lg:h-[200px] lg:w-[320px] ${description[index] ? 'h-[300px] lg:h-[250px] lg:w-[400px]' : ''}`}
           style={{ backgroundImage: `url(${servicio.imagen})` }}
         >
@@ -31,20 +31,33 @@ const TarjetaServicios = ({ tratamientos }) => {
             className={`absolute inset-0 left-0 top-0 rounded-lg bg-black/50 ${description[index] ? 'bg-black' : ''} `}
           />
 
-          <div className="relative z-10 text-center">
+          <div className="relative z-10 flex h-full w-full items-center justify-center text-center">
             {description[index] ? (
-              <div className="mx-4 border-[1px] border-white bg-white/20 p-2">
+              <div>
+                <div className="mx-4 border-[1px] border-white bg-white/20 p-2">
+                  <div
+                    onClick={() => handleCloseClick(index)}
+                    className="flex h-auto justify-end pr-3"
+                  >
+                    <FiX size={28} color="#fff" />
+                  </div>
+                  <p className="font-sourceSerif text-lg font-normal text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">
+                    {servicio.titulo}
+                  </p>
+                  <p className="mx-3 text-justify font-mulish text-sm font-normal text-white">
+                    {servicio.texto}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <div
+                onClick={() => handleClickEnter(index)}
+                className="flex h-full w-full items-center justify-center"
+              >
                 <p className="font-sourceSerif text-lg font-normal text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">
                   {servicio.titulo}
                 </p>
-                <p className="mx-3 text-justify font-mulish text-sm font-normal text-white">
-                  {servicio.texto}
-                </p>
               </div>
-            ) : (
-              <p className="font-sourceSerif text-lg font-normal text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">
-                {servicio.titulo}
-              </p>
             )}
           </div>
         </div>
