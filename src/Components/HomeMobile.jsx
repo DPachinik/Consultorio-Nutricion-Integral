6aa79f06cd635e7d4911a54da007bcb2beffcb32
David Pachinik <ing.pachinik96@gmail.com>
2025-08-04 21:03:33 -0400

    feat(ui):restyle component

--- src/Components/HomeMobile.jsx
+++ src/Components/HomeMobile.jsx
@@ -28,11 +28,11 @@ const HomeMobile = () => {
         className="relative flex h-[170px] min-h-[150px] w-[170px] min-w-[150px] flex-col rounded-full bg-alimentos bg-cover bg-center shadow-[0_0_40px_theme('colors.lime.600')] ring-1 ring-lima"
         data-aos="fade-left"
       >
-        <div className="absolute -left-4 top-[30px] w-fit rounded-2xl bg-lima px-3 font-mulish text-[18px] text-white">
+        <div className="absolute -left-12 top-[30px] w-fit rounded-2xl bg-lima px-3 font-mulish text-[18px] text-white">
           + Resultados
         </div>
 
-        <div className="absolute bottom-[30px] right-0 w-fit rounded-2xl bg-lima px-3 font-mulish text-[18px] text-white">
+        <div className="absolute -right-5 bottom-[30px] w-fit rounded-2xl bg-lima px-3 font-mulish text-[18px] text-white">
           + Salud
         </div>
       </div>
